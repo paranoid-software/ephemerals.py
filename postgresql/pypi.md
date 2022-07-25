009841e28e7c318e2cc560c0839e8e1efa6babc4
Pablo Pinargote <pablo.pinargote@outlook.com>
2022-07-25 08:21:50 -0500

    Ephemeral PostgreSql Db Context Helper (Alpha)

---
+++ postgresql/pypi.md
@@ -0,0 +1,3 @@
+# Ephemeral PostgreSQL Database Context
+
+Ephemeral database context for PostgreSQL, it can be used to code integration tests with a fairly isolated database environment.
--- postgresql/tests/test_initializing_context_should.py
+++ postgresql/tests/test_initializing_context_should.py
@@ -1,21 +1,21 @@
 import pytest
 from assertpy import assert_that
 
-from mssql import EphemeralMsSqlDbContextBuilder
+from postgresql import EphemeralPostgreSqlDbContextBuilder
 
 
 class TestInitializingContextShould:
 
     def test_throw_exception_when_data_source_is_not_ocal(self):
         with pytest.raises(Exception) as e:
-            with EphemeralMsSqlDbContextBuilder()\
-                    .build('DRIVER={ODBC Driver 17 for SQL Server};Server=10.142.1.11;UID=sa;PWD=my-New-pwd;'):
+            with EphemeralPostgreSqlDbContextBuilder()\
+                    .build('Server=192.168.1.1;Port=35432;'):
                 pass
         assert_that(str(e.value)).is_equal_to('Ephemeral database server must be local, use localhost or 127.0.0.1 as server address.')
 
     def test_throw_exception_when_database_name_is_present(self):
         with pytest.raises(Exception) as e:
-            with EphemeralMsSqlDbContextBuilder()\
-                    .build('DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost,31433;UID=sa;PWD=my-New-pwd;DATABASE=mydb'):
+            with EphemeralPostgreSqlDbContextBuilder()\
+                    .build('Server=localhost;Port=35432;Database=my_database;'):
                 pass
         assert_that(str(e.value)).is_equal_to('Ephemeral database name should not be included on the connection string, please remove DATABASE parameter.')
