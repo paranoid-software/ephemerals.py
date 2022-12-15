5effb99409fde3047ac2383da8a0cf780349b596
Pablo Pinargote <pablo.pinargote@outlook.com>
2022-12-15 14:41:19 -0500

    Ephemeral MongoDB Context Helper

--- rocket/ephemeralsrocket/__init__.py
+++ rocket/ephemeralsrocket/__init__.py
@@ -4,4 +4,4 @@ from .dbmanager import DbManagerProtocol, DbManager
 from .ephemeralrocketdbcontext import EphemeralRocketDbContext
 from .ephemeralrocketdbcontextbuilder import EphemeralRocketDbContextBuilder
 
-__version__ = "1.0.3"
+__version__ = '1.0.3'
